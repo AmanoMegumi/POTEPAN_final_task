6142a249c74cf187e66fbb85f23c8a53164ab557
Megumi <suz34000@gmail.com>
2022-09-20 16:08:29 +0900

    rspecのエラー解消

--- spec/requests/potepan/products_spec.rb
+++ spec/requests/potepan/products_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
+require 'spree/testing_support/factories'
 
 RSpec.describe "Potepan::Products", type: :request do
-  describe "GET /show" do
+  describe "GET #show" do
+    let(:product) { create(:product) }
+
     it "returns http success" do
-      get "/potepan/products/show"
-      expect(response).to have_http_status(:success)
+      get potepan_product_path(product.id)
+      expect(response).to have_http_status(200)
     end
   end
 end
