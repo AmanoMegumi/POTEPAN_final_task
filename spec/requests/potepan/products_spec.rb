b2a3bee52238ccd5202e56cfa7267e9976e0e07b
Megumi <suz34000@gmail.com>
2022-09-21 14:29:27 +0900

    showビューの値段表示修正、テスト修正

--- spec/requests/potepan/products_spec.rb
+++ spec/requests/potepan/products_spec.rb
@@ -4,10 +4,22 @@ require 'spree/testing_support/factories'
 RSpec.describe "Potepan::Products", type: :request do
   describe "GET #show" do
     let(:product) { create(:product) }
+    before do
+      get potepan_product_path(product.id)
+    end
 
     it "returns http success" do
-      get potepan_product_path(product.id)
       expect(response).to have_http_status(200)
+    end
+
+    it "shows the product information" do
+      expect(response.body).to include(product.name)
+      expect(response.body).to include(product.display_price.to_s)
+      expect(response.body).to include(product.description)
+    end
+
+    it "shows the product images" do
+      product.images.each { |image| expect(response.body).to include image.attachment(:product) }
     end
   end
 end
