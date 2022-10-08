8b3d8913e1ed46f10e8b1dcf93e35f3883d488b3
Megumi <suz34000@gmail.com>
2022-10-08 12:11:46 +0900

    PR指摘箇所の修正

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 RSpec.describe ApplicationHelper, type: :helper do
   base_title = "BIGBAG Store"
-  let(:product) { create(:product) }
 
   example "the argument is set" do
-    expect(full_title(product.name)).to eq("#{product.name} - #{base_title}")
+    expect(full_title("test")).to eq("test - #{base_title}")
   end
 
   example "the argument is null or empty" do
