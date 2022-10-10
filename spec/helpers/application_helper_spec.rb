c5d5a8e17eaac9c1785801f3e2d0f50f381533d4
Megumi <suz34000@gmail.com>
2022-10-10 22:35:23 +0900

    rubocopエラー解消（定数定義を変更）

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 RSpec.describe ApplicationHelper, type: :helper do
-  BASE_TITLE = "BIGBAG Store"
+  describe "full_title(page_title)" do
+    example "the argument is set" do
+      expect(full_title("test")).to eq("test - BIGBAG Store")
+    end
 
-  example "the argument is set" do
-    expect(full_title("test")).to eq("test - #{BASE_TITLE}")
-  end
+    example "the argument is null" do
+      expect(full_title(nil)).to eq "BIGBAG Store"
+    end
 
-  example "the argument is null" do
-    expect(full_title(nil)).to eq BASE_TITLE
-  end
-
-  example "the argument is empty" do
-    expect(full_title('')).to eq BASE_TITLE
+    example "the argument is empty" do
+      expect(full_title('')).to eq "BIGBAG Store"
+    end
   end
 end
